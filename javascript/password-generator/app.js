d0e0a8cd182077b8bc7fa0ae485ae4ed74e8661a
enes <ensbzlk@gmail.com>
2024-03-10 16:47:27 +0300

    Password Generator

---
+++ javascript/password-generator/app.js
@@ -0,0 +1,14 @@
+const generate = len => {
+    chars = [];
+    for (let i = 33; i <= 126; i++) {
+        chars.push(i);
+    }
+    chars = chars.map(n => String.fromCharCode(n));
+
+    let result = "";
+    for (let i = 0; i < len; i++) {
+        const randomN = Math.floor(Math.random() * chars.length);
+        result += chars[randomN];
+    }
+    console.log(result);
+}
